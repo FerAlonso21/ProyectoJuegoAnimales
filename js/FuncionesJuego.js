4b05672bb53ffc32e90ef843fceaa7f3721e3501
Feer-Cano <feernando_cano@hotmail.com.com>
2022-03-07 23:22:24 -0600

    adasdasd

--- js/FuncionesJuego.js
+++ js/FuncionesJuego.js
@@ -2,7 +2,8 @@ var lista = new Array();
 function principal(){
     
     var animales = new Array();
-    var habitat = new Array();    
+    var habitat = new Array(); 
+      
     var ban1=true;
     var cont=1,cont2=0,ale;
     while (ban1 == true) {//  es solamente una bandera para que entre al ciclo 
@@ -28,18 +29,34 @@ function principal(){
     //AQUI  ya esta el vector con los numeros aleatorios
     //Aqui se debe de asignar las imagenes a los divs
     console.log(lista);
-    
+    var aux2 = new Array(); 
+    var aux;
     for (let i = 0; i <= 2; i++) {
-        document.getElementById("animal"+i).src="./imagenes/Animales/animal"+lista[i]+".png";
-        document.getElementById("habita"+i).src="./imagenes/Ecosistemas/habitat"+lista[i]+".png";
+      ale = Math.floor(Math.random() * (2 - 0 + 1)) + 0;
+        if(i==0){
+          
+          aux=ale;
+          aux2.push(aux);
+        }else{
+          if(ale!=aux){
+            aux=ale;
+            aux2.push(aux);
+          }
+        }
+        if(i==2){
+           aux=aux2[0]+aux2[1];
+           
+        }
+        document.getElementById("animal"+aux).src="./imagenes/Animales/animal"+lista[i]+".png";
+        document.getElementById("habitat"+aux).src="./imagenes/Ecosistemas/habitat"+lista[i]+".png";
     }
     
 }
 
 function boton(){
-    /*for (let i = 3; i < 5; i++) {
+    for (let i = 3; i < 5; i++) {
         document.getElementById("animal"+i).src="./imagenes/animal"+lista[i]+".PNG";
         document.getElementById("habita"+i).src="./imagenes/habita"+lista[i]+".PNG";
-    }*/
+    }
 }
 window.onload= principal;
