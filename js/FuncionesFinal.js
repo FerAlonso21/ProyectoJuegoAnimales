2672ef49abc5d68fa0a4d41b1347a8faf6d27b79
Feer-Cano <feernando_cano@hotmail.com.com>
2022-03-07 22:46:11 -0600

    nuevosa

---
+++ js/FuncionesFinal.js
@@ -0,0 +1,6 @@
+function hola(){
+
+    alert("n9 askoas");
+
+  
+}
--- js/FuncionesJuego.js
+++ js/FuncionesJuego.js
@@ -2,8 +2,7 @@ var lista = new Array();
 function principal(){
     
     var animales = new Array();
-    var habitat = new Array();
-    
+    var habitat = new Array();    
     var ban1=true;
     var cont=1,cont2=0,ale;
     while (ban1 == true) {//  es solamente una bandera para que entre al ciclo 
@@ -24,19 +23,16 @@ function principal(){
               }
               if (lista.length > 5) {//validamos si ya esta lleno el array de numeros aleatorios
                 ban1 = false;//switcheamos la bandera para que no entre al while
-            }
-        
-        
-        
+            }  
     }
-   
     //AQUI  ya esta el vector con los numeros aleatorios
     //Aqui se debe de asignar las imagenes a los divs
     console.log(lista);
-    /*for (let i = 0; i < 2; i++) {
-        document.getElementById("animal"+i).src="./imagenes/animal"+lista[i]+".PNG";
-        document.getElementById("habita"+i).src="./imagenes/habita"+lista[i]+".PNG";
-    }*/
+    
+    for (let i = 0; i <= 2; i++) {
+        document.getElementById("animal"+i).src="./imagenes/Animales/animal"+lista[i]+".png";
+        document.getElementById("habita"+i).src="./imagenes/Ecosistemas/habitat"+lista[i]+".png";
+    }
     
 }
 
